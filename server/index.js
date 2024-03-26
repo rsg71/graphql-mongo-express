8f52cfe85c544daa3f909e1b8506d1966e8f192b
Robert Greenawalt <robert.greenawalt29@gmail.com>
2024-03-25 22:04:32 -0400

    Remove unused vars

--- server/index.js
+++ server/index.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const { graphqlHTTP } = require('express-graphql');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const colors = require('colors');
+require('colors');
 const connectDB = require('./config/db');
 const schema = require('./schema/schema');
 
